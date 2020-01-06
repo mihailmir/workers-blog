2d26621b7d7164baeada01d75d3e2a14aa291b2e
Mikhail Miroshnichenko <mikhail.miroshnichenko18@gmail.com>
2020-01-06 11:29:15 +0200

    Update settings.py
    removed secret key

--- workers/settings.py
+++ workers/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ifs7!1@6dl7eanz(gw()$mp6--*55j(28ze6^d%ry8qx8kh7)('
+SECRET_KEY = '*****'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
